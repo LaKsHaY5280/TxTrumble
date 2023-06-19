5b44920fccab4d424f60fd3ce4fb6bd82c1c78c6
LaKsHaY5280 <130125619+LaKsHaY5280@users.noreply.github.com>
2023-06-19 14:06:48 +0530

    added history

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function App() {
     setTimeout(() => {
       setalert(null);
     }, 1250);
-    
 
   }
   
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,6 @@ export default function Navbar(props) {
                     </div>
                 </div>
             </nav>
-            
         </div>
   )
 }
