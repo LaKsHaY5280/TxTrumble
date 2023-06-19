f978d3cc737772141c6246666d721f343e91af6d
LaKsHaY5280 <130125619+LaKsHaY5280@users.noreply.github.com>
2023-06-19 14:23:47 +0530

    .

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -13,25 +13,15 @@ export default function Alert(props) {
 
 
     return (
-<<<<<<< HEAD
         <div style={{ height: '60px' }} >{
             props.alert &&
       <div>
-=======
-        <div style={{ height: '50px'}} >{
-            props.alert &&
-            <div>
->>>>>>> f0248fae949052d217e6e693f223791bfdb2e7b5
           <div className={`alert alert-${props.alert.type } alert-dismissible fade show`} role="alert">
               <strong>{Capitalize(props.alert.type)}   </strong> {props.alert.msg }
               <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
       </div>
-<<<<<<< HEAD
     }
         </div>
-=======
-    }</div>
->>>>>>> f0248fae949052d217e6e693f223791bfdb2e7b5
   )
 }
